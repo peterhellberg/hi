4f38478f3c4a283e9131f55dc1fe92c080d4ea7c
Peter Hellberg <peter@c7.se>
2016-11-11 20:56:51 +0100

    Added JSON output for the hi command

--- cmd/hi/main.go
+++ cmd/hi/main.go
@@ -1,40 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/peterhellberg/hi"
 )
 
-var (
-	limit   = flag.Int("l", 3, "limit number of images")
-	shuffle = flag.Bool("s", true, "shuffle the images")
-)
-
 func main() {
+	limit := flag.Int("l", 3, "limit number of images")
+	shuffle := flag.Bool("s", false, "shuffle the images")
+	jsonOutput := flag.Bool("json", false, "set output format to JSON")
+
 	flag.Parse()
 
 	hashtag := flag.Arg(0)
 
 	if hashtag == "" {
 		fmt.Println("Missing hashtag")
-		return
+		os.Exit(1)
 	}
 
-	findImages := hi.FindImages
+	var (
+		limitedImages = []hi.Image{}
+		findImages    = hi.FindImages
+	)
 
 	if *shuffle {
 		findImages = hi.FindShuffledImages
 	}
 
 	if images, err := findImages(hashtag); err == nil {
-		for i, img := range images {
-			fmt.Println(img.URL)
+		l := *limit
 
-			if i == *limit-1 {
+		for i, img := range images {
+			if i == l {
 				break
 			}
+
+			limitedImages = append(limitedImages, img)
+		}
+	}
+
+	if *jsonOutput {
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		enc.Encode(limitedImages)
+	} else {
+		for _, img := range limitedImages {
+			fmt.Println(img.URL)
 		}
 	}
 }
